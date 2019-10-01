641d3fdf655eefac5158d657ae01144663efbd7c
ravi <ravi@drupaltechie.com>
2019-10-01 18:22:01 +0530

    Update 7.59

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
  * Root directory of Drupal installation.
  */
 define('DRUPAL_ROOT', getcwd());
-
+error_reporting( E_ERROR );
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
